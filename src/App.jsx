ea7ce7da35005236f414a313890ee6a457ba16a9
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-05-15 21:25:21 -0500

    feat: add Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { StrictMode, useState } from 'react'
-import { createRoot } from 'react-dom/client'
-import './App.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./App.css";
+import Header from "./components/Header";
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      	
+      <Header />
     </>
-  )
+  );
 }
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div id="header">
+        <h1>Memory Card Game</h1>
+        <p>Get points by clicking on an gif only once</p>
+    </div>
+  );
+}
