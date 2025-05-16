8d3081221d0ee0a35b8d9130a5ff9cd0675b49a2
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-05-15 21:33:08 -0500

    feat: add ScoreTable.jsx into Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import ScoreTable from "./ScoreTable";
 export default function Header() {
   return (
     <div id="header">
-        <h1>Memory Card Game</h1>
-        <p>Get points by clicking on an gif only once</p>
+        <div className="main-text">
+            <h1>Memory Card Game</h1>
+            <p>Get points by clicking on an gif only once</p>
+        </div>
+        <ScoreTable />
     </div>
   );
 }
---
+++ src/components/ScoreTable.jsx
@@ -0,0 +1,14 @@
+export default function ScoreTable() {
+  return (
+    <table>
+        <thead>
+            <th>Score</th>
+            <th>Best Score</th>
+        </thead>
+        <tbody>
+            <td>0</td>
+            <td>1</td>
+        </tbody>
+    </table>
+  );
+}
