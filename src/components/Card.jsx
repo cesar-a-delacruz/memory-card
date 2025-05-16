2285cc0f8993fb5b39a9375f1315933e2f201336
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-05-15 21:57:16 -0500

    feat: add Card.jsx into CardGrid.jsx

---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card() {
+  return (
+    <div className="card">
+      <img src="" alt="" />
+      <p></p>
+    </div>
+  );
+}
--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,7 +1,8 @@
+import Card from "./Card";
 export default function CardGrid() {
-    return (
-        <div id="card-grid">
-            
-        </div>
-    );
+  const cards = [];
+  for (let i = 0; i < 15; i++) {
+    cards[i] = <Card />;
+  }
+  return <div id="card-grid">{cards.map((card) => card)}</div>;
 }
